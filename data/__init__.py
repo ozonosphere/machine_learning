0bd34b679972b5369d65234771bb8e1396bdb8d6
Charles <lixianzhe0112@gmail.com>
2019-06-26 16:14:16 +0100

    updated

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,18 @@
 import pandas
+from contextlib import contextmanager
+import os
 
-def get_data(path):
-    data = pandas.read_csv(path, header=None)
+@contextmanager
+def change_working_directory(path):
+    old_pwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(old_pwd)
+
+def get_data(file_name):
+    dirname = os.path.dirname(__file__)
+    full_file_path = os.path.join(dirname, file_name)
+    data = pandas.read_csv(full_file_path, header=None)
     return data
--- useful_wrapper_functions.py
+++ useful_wrapper_functions.py
@@ -3,20 +3,20 @@ import numpy
 from sklearn import linear_model
 
 
-def fit_GDregression(x, y, n_iter=100, alpha=0.0001,typ=None, los=None):
+def fit_GDregression(x, y, typ=None):
     '''
     Gradient descent algo that returns a numpy array (predicted Y)
     '''
     if typ is None:
-        regression = linear_model.SGDRegressor(n_iter=n_iter, alpha=alpha, loss=los)
+        regression = linear_model.SGDRegressor(loss='squared_loss')
         regression.fit(x,y)
         coef_array = numpy.hstack((regression.intercept_, regression.coef_))
         linear_func = numpy.poly1d(coef_array[::-1])
         print(numpy.poly1d(linear_func))
         print('R squre is: ' + str(regression.score(x,y)))
         return regression.predict(x)
-    elif typ == 'log':
-        regression = linear_model.SGDClassifier(n_iter=n_iter, alpha=alpha, loss=los)
+    elif typ == 'logistic':
+        regression = linear_model.SGDClassifier(loss='log')
         regression.fit(x,y)
         print('accuracy is: ' + str(regression.score(x,y)))
         return regression
