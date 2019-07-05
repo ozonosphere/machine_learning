620239ecbd2a7363cfe87e197d788ca953d12405
Charles <lixianzhe0112@gmail.com>
2019-07-05 16:44:41 +0100

    fixed some bugs and added some TODOs

--- algos/__init__.py
+++ algos/__init__.py
@@ -34,7 +34,7 @@ def convert_data_to_dict_of_array(data_tuple: tuple) -> dict:
     training_x_variable_array = training_data_set.values[:, :-1]
     testing_x_variable_array = testing_data_set.values[:, :-1]
     testing_y_variables = testing_data_set['y'].values
-    return {'x_variables': training_x_variable_array, 'y_variables': y_variables,
+    return {'training_x_variables': training_x_variable_array, 'training_y_variables': y_variables,
             'y_variables_by_class': training_y_variable_arrays, 'testing_x_variables': testing_x_variable_array,
             'testing_y_variables': testing_y_variables}
 
@@ -45,13 +45,13 @@ class MachineLearningAlgos(object):
 class LogisticRegression(MachineLearningAlgos):
     # This Regression is specifically designed for classification as the cost function is the log function (convex function)
     # The cost function can be others to solve different problems
-    def __init__(self, x_variables: array, y_variables: array, y_variables_by_class: dict, learning_rate: float, testing_x_variables: array=None, testing_y_variables: array=None,
-                 regularized_lambda: float=0.0, number_of_iteration: int=1000):
+    def __init__(self, training_x_variables: array, training_y_variables: array, y_variables_by_class: dict, learning_rate: float, testing_x_variables: array=None, testing_y_variables: array=None,
+                 regularized_lambda: float=0.0, number_of_iteration: int=1000, **unused):
         super().__init__()
         self.y_variables_by_class = y_variables_by_class # dictionary of class index with corresponding y_variables array (binary, 1 and 0)
         self.number_of_class = len(self.y_variables_by_class)
-        self.x_variables = x_variables
-        self.y_variables = y_variables # 1d array (number of datapoints, )
+        self.training_x_variables = training_x_variables
+        self.training_y_variables = training_y_variables # 1d array (number of datapoints, )
         self.testing_x_variables = testing_x_variables
         self.testing_y_variables = testing_y_variables
         self.regularized_lambda = regularized_lambda
@@ -61,7 +61,7 @@ class LogisticRegression(MachineLearningAlgos):
 
     def initialising_weights(self):
         self.optimal_weights_dict_by_class = {}
-        weight_vector_shape = self.x_variables[0, :].shape # shape (number of features, )
+        weight_vector_shape = self.training_x_variables[0, :].shape # shape (number of features, )
         for class_ in self.y_variables_by_class:
             self.optimal_weights_dict_by_class[class_] = numpy.random.uniform(size=(weight_vector_shape[0], 1))
 
@@ -75,7 +75,7 @@ class LogisticRegression(MachineLearningAlgos):
         return 1 / (1 + numpy.exp(-(x_variables.dot(weights))))
 
     def eval_cost_function(self, weights: array, y_variables: array) -> float:
-        probability_of_each_sample = self.eval_sigmoid_function(self.x_variables, weights) # shape (number of datapoints, 1)
+        probability_of_each_sample = self.eval_sigmoid_function(self.training_x_variables, weights) # shape (number of datapoints, 1)
         a = numpy.log(1 - probability_of_each_sample)
         numpy.place(a, a == -numpy.inf, 0.0)
         b = numpy.log(probability_of_each_sample)
@@ -90,9 +90,9 @@ class LogisticRegression(MachineLearningAlgos):
         """
         :return: weights partial derivatives with shape (number of features, 1)
         """
-        costs = self.eval_sigmoid_function(self.x_variables, weights)
+        costs = self.eval_sigmoid_function(self.training_x_variables, weights)
         number_of_datapoints = len(y_variables)
-        derivatives = 1 / number_of_datapoints * (self.x_variables.transpose().dot((costs - y_variables))) # shape (number of features, 1)
+        derivatives = 1 / number_of_datapoints * (self.training_x_variables.transpose().dot((costs - y_variables))) # shape (number of features, 1)
         regularized_terms = self.regularized_lambda / number_of_datapoints * weights
         regularized_terms[0, :] = 0 # first feature weight is bias term if regularized_lambda is given
         derivatives = derivatives + regularized_terms
@@ -122,10 +122,10 @@ class LogisticRegression(MachineLearningAlgos):
     def train(self):
         self.initialising_weights()
         self.get_optimal_weights_by_gradient_decent()
-        prob = self.get_prob_for_each_class_by_optimal_weights(self.x_variables)
+        prob = self.get_prob_for_each_class_by_optimal_weights(self.training_x_variables)
         self.training_probability_by_class_index_df = prob
         self.training_predicted_y = self.training_probability_by_class_index_df.idxmax(axis='columns').values
-        self.training_accuracy = numpy.sum(self.y_variables == self.training_predicted_y) / len(self.y_variables)
+        self.training_accuracy = numpy.sum(self.training_y_variables == self.training_predicted_y) / len(self.training_y_variables)
         print('Training accuracy is: ' + str(self.training_accuracy))
 
     def test(self):
@@ -140,16 +140,18 @@ class LogisticRegression(MachineLearningAlgos):
 
 
 class NeuralNetwork(MachineLearningAlgos):
-    def __init__(self, no_of_nodes_in_layers: dict, input_layer_x_variables: array,
-                 output_layer_y_variables: array, learning_rate: float, testing_x_variables: array=None,
-                 testing_y_variables: array=None, regularized_lambda: float=0.0, cost_function: str='log'):
+    def __init__(self, layer_structure: dict, training_x_variables: array,
+                 training_y_variables: array, learning_rate: float, no_of_training_batches: int=1, testing_x_variables: array=None,
+                 testing_y_variables: array=None, regularized_lambda: float=0.0, cost_function: str='log', **unused):
         super().__init__()
-        self.number_of_output_classes = len(numpy.unique(output_layer_y_variables))
-        self.number_of_layers = max(no_of_nodes_in_layers.keys())
-        self.no_of_nodes_in_layers = no_of_nodes_in_layers #s structure of {layer number: number of nodes (excluding bias), ...}
-        self.no_of_nodes_in_layers[self.number_of_layers] = self.number_of_output_classes
-        self.input_layer_x_variables = input_layer_x_variables
-        self.output_layer_y_variables = output_layer_y_variables  # 1d array (number of datapoints, )
+        self.no_of_training_batches = no_of_training_batches
+        self.number_of_output_classes = len(numpy.unique(training_y_variables))
+        self.number_of_layers = max(layer_structure.keys())
+        self.layer_structure = layer_structure # structure of {layer number: number of nodes (excluding a(l, 0), ...}
+        self.training_x_variables = training_x_variables
+        self.training_y_variables = training_y_variables  # 1d array (number of datapoints, )
+        self.layer_structure[1] = self.training_x_variables.shape[1]
+        self.layer_structure[self.number_of_layers] = self.number_of_output_classes
         self.testing_x_variables = testing_x_variables
         self.testing_y_variables = testing_y_variables
         self.regularized_lambda = regularized_lambda
@@ -158,67 +160,81 @@ class NeuralNetwork(MachineLearningAlgos):
         self.optimal_weights_by_layer_number = {}
         self.bias_terms_by_layer_number = {}
         # Activation values by layer number with bias appended
-        self.activation_values_by_layer_number = {1: self.add_bias_ones_to_activations(input_layer_x_variables, numpy.ones((input_layer_x_variables.shape[0], 1)))}
+        self.activation_values_by_layer_number = {}
+        self.activation_values_including_ones_by_layer_number = {}
         self.error_by_layer_number = {}
         self.derivatives_of_cost_vs_weights_by_layer = {}
         self.activation_derivatives_by_layer_number = {}
+        self.slice_training_data_into_batches()
+        self.initializing_weights()
 
-    def add_bias_ones_to_activations(self, array, bias_term_array):
+    def slice_training_data_into_batches(self):
+        self.input_layer_x_variables_by_batch = {}
+        self.output_layer_y_variables_by_batch = {}
+        x_variable_batches = numpy.array_split(self.training_x_variables, self.no_of_training_batches)
+        y_variable_batches = numpy.array_split(self.training_y_variables, self.no_of_training_batches)
+        for batch_number in range(self.no_of_training_batches):
+            self.input_layer_x_variables_by_batch[batch_number + 1] = x_variable_batches[batch_number]
+            self.output_layer_y_variables_by_batch[batch_number + 1] = y_variable_batches[batch_number]
+
+    def append_ones_to_activation(self, x_variables: array):
+        self.activation_values_including_ones_by_layer_number[1] = self.add_bias_ones_to_activations(x_variables)
+        self.activation_values_by_layer_number[1] = x_variables
+
+    def add_bias_ones_to_activations(self, array):
         # array with shape (number of datapoints, number of features)
-        return numpy.append(array[:, ::-1], bias_term_array, axis=1)[:, ::-1]
+        ones_array = numpy.ones((array.shape[0], 1))
+        return numpy.append(array[:, ::-1], ones_array, axis=1)[:, ::-1]
 
     def initializing_weights(self):
-        for layer_number, no_of_nodes in self.no_of_nodes_in_layers.items():
-            self.bias_terms_by_layer_number[layer_number] = numpy.ones((no_of_nodes, 1))
+        for layer_number, no_of_nodes in self.layer_structure.items():
             if layer_number == 1:
                 pass
             else:
-                no_of_nodes_previous_layer = self.no_of_nodes_in_layers[layer_number - 1]
-                self.optimal_weights_by_layer_number[layer_number] = numpy.random.randn(no_of_nodes, no_of_nodes_previous_layer)\
-                                                                     * numpy.sqrt(2 / (no_of_nodes + no_of_nodes_previous_layer))
+                no_of_nodes_previous_layer = self.layer_structure[layer_number - 1]
+                self.optimal_weights_by_layer_number[layer_number] = numpy.random.randn(no_of_nodes, no_of_nodes_previous_layer + 1)\
+                                                                     * numpy.sqrt(2 / (no_of_nodes + no_of_nodes_previous_layer + 1))
+                self.bias_terms_by_layer_number[layer_number] = numpy.zeros((self.input_layer_x_variables_by_batch[1].shape[0], no_of_nodes))
 
     def eval_activations_by_feeding_forward(self):
         for layer_number in range(2, self.number_of_layers + 1):
-            bias_appended_weights = self.add_bias_ones_to_activations(self.optimal_weights_by_layer_number[layer_number],
-                                                                      self.bias_terms_by_layer_number[layer_number])
-            self.activation_values_by_layer_number[layer_number] = self.eval_activation_values(self.activation_values_by_layer_number[layer_number - 1], bias_appended_weights)
+            self.activation_values_by_layer_number[layer_number] = self.eval_activation_values(self.activation_values_including_ones_by_layer_number[layer_number - 1],
+                                                                                               self.optimal_weights_by_layer_number[layer_number],
+                                                                                               self.bias_terms_by_layer_number[layer_number])
+            if layer_number != self.number_of_layers:
+                self.activation_values_including_ones_by_layer_number[layer_number] = self.add_bias_ones_to_activations(self.activation_values_by_layer_number[layer_number])
+            else:
+                self.activation_values_including_ones_by_layer_number[layer_number] = self.activation_values_by_layer_number[layer_number]
+            self.activation_derivatives_by_layer_number[layer_number] = self.eval_activation_derivatives(self.activation_values_including_ones_by_layer_number[layer_number])
 
-    def eval_activation_derivatives_by_level(self):
-        for layer_number in range(2, self.number_of_layers + 1):
-            bias_appended_weights = self.add_bias_ones_to_activations(self.optimal_weights_by_layer_number[layer_number],
-                                                                      self.bias_terms_by_layer_number[layer_number])
-            self.activation_derivatives_by_layer_number[layer_number] = self.eval_activation_derivatives(self.activation_values_by_layer_number[layer_number - 1],
-                                                                                                         bias_appended_weights)
 
     def eval_errors_by_back_propagation(self):
         for layer_number in reversed(range(2, self.number_of_layers)):
-            self.error_by_layer_number[layer_number] = self.optimal_weights_by_layer_number[layer_number + 1].T.\
-                dot(self.error_by_layer_number[layer_number + 1]) * self.activation_derivatives_by_layer_number[layer_number]
+            self.error_by_layer_number[layer_number] = self.error_by_layer_number[layer_number + 1].\
+                dot(self.optimal_weights_by_layer_number[layer_number + 1]) * self.activation_derivatives_by_layer_number[layer_number]
 
     def eval_cost_derivatives_vs_weights(self):
         for layer_number in range(2, self.number_of_layers + 1):
-            self.derivatives_of_cost_vs_weights_by_layer[layer_number] = self.error_by_layer_number[layer_number + 1].\
-                dot(self.optimal_weights_by_layer_number[layer_number + 1]) * self.activation_derivatives_by_layer_number[layer_number]
+            self.derivatives_of_cost_vs_weights_by_layer[layer_number] = (self.activation_values_including_ones_by_layer_number[layer_number - 1].
+                                                                          T.dot(self.error_by_layer_number[layer_number])).T
 
-    def eval_activation_values(self, activation_last_layer, weights_current_layer: array) -> array:
+    def eval_activation_values(self, activation_last_layer, weights_current_layer: array, bias_current_layer: array) -> array:
         '''
         :param weights_current_layer: shape of (number of nodes in current layer, number of nodes in the previous layer) 2d array
         :param activation_last_layer: shape of (number of datapoints, number of nodes in the previous layer)
+        :param bias_current_layer: shape of (number of datapoints, number of nodes in the current layer)
         :return: a 2d array with shape (number of datapoints, number of nodes in the current layer) representing activation values at current layer
         '''
-        return 1 / (1 + numpy.exp(-(activation_last_layer.dot(weights_current_layer.T))))
+        return 1 / (1 + numpy.exp(-(activation_last_layer.dot(weights_current_layer.T) + bias_current_layer)))
 
-    def eval_activation_derivatives(self, activation_previous_layer: array, weights_current_layer: array) -> array:
-        activation_value = self.eval_activation_values(activation_previous_layer, weights_current_layer)
+    def eval_activation_derivatives(self, activation_value: array) -> array:
         return activation_value * (1 - activation_value)
 
-    def eval_output_layer_error(self, activation_previous_layer: array, weights_output_layer: array, y_variables: array) -> array:
+    def eval_output_layer_error(self, y_variables: array) -> array:
         """
         :return: shape of (number of datapoints, number of classes)
         """
-        bias_appended_weights = self.add_bias_ones_to_activations(weights_output_layer,
-                                                                  self.bias_terms_by_layer_number[self.number_of_layers])
-        activation_value_output_layer = self.eval_activation_values(activation_previous_layer, bias_appended_weights)
+        activation_value_output_layer = self.activation_values_by_layer_number[self.number_of_layers]
         activation_derivatives_output_layer = self.activation_derivatives_by_layer_number[self.number_of_layers]
         cost_vs_output_activation_derivatives = self.eval_derivative_of_cost_vs_output_activation(activation_value_output_layer, y_variables)
         result = cost_vs_output_activation_derivatives * activation_derivatives_output_layer
@@ -244,14 +260,44 @@ class NeuralNetwork(MachineLearningAlgos):
         else:
             raise NotImplementedError('Cannot compute derivative for cost vs activation, reason: unknown cost function type.')
 
+    def update_weights_and_errors_by_batch(self, batch_number: int):
+        self.append_ones_to_activation(self.input_layer_x_variables_by_batch[batch_number])
+        self.eval_activations_by_feeding_forward()
+        self.eval_output_layer_error(self.output_layer_y_variables_by_batch[batch_number])
+        self.eval_errors_by_back_propagation()
+        self.eval_cost_derivatives_vs_weights()
+        learning_rate = self.learning_rate / self.input_layer_x_variables_by_batch[batch_number].shape[0]
+        for layer_number in range(2, self.number_of_layers + 1):
+            self.optimal_weights_by_layer_number[layer_number] = self.optimal_weights_by_layer_number[layer_number] - \
+                                                                 learning_rate * self.derivatives_of_cost_vs_weights_by_layer[layer_number]
+            self.bias_terms_by_layer_number[layer_number] = self.bias_terms_by_layer_number[layer_number] - \
+                                                            learning_rate * self.error_by_layer_number[layer_number]
+
+    def update_batches(self):
+        for batch_number in self.input_layer_x_variables_by_batch:
+            self.update_weights_and_errors_by_batch(batch_number)
+
     def train(self):
-        pass
+        self.update_batches() # Getting optimal weights and bias
+        # ================ Using the entire training set to evaluate output activation =====================
+        self.append_ones_to_activation(self.training_x_variables)
+        self.eval_activations_by_feeding_forward()
+        # ==================================================================================================
+        self.output_layer_training_activation = self.activation_values_by_layer_number[self.number_of_layers]
+        self.training_y_results = self.output_layer_training_activation.argmax(axis=1) + 1
+        accuracy = numpy.sum(self.training_y_variables == self.training_y_results) / len(self.training_y_variables)
+        print('Training accuracy: ' + str(accuracy))
 
     def test(self):
-        pass
+        self.append_ones_to_activation(self.testing_x_variables)
+        self.eval_activations_by_feeding_forward()
+        self.output_layer_testing_activation = self.activation_values_by_layer_number[self.number_of_layers]
+        self.testing_y_results = self.output_layer_testing_activation.argmax(axis=1) + 1
+        accuracy = numpy.sum(self.testing_y_variables == self.testing_y_results) / len(self.testing_y_variables)
+        print('Testing accuracy: ' + str(accuracy))
 
-
-
+        #TODO figure out the bias term implications, whether to include them dynamically in the activations and weights or keep a bias term.
+        # Keeping a bias term seem to be not working with multiple batch update...
 
 if __name__ == '__main__':
     training_set_lenth = 4000
